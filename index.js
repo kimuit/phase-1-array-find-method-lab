1fd54a610d29b742f29681061ee1a3cd2109049f
kimuit117 <kemoikiuita@gmail.com>
2022-08-27 15:30:44 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+
+function superbowlWin(record){
+    let result = record.find(i=>i.result == "W")
+
+    if(result){
+        return result.year
+    }else{
+        return result
+    }
+
+}
